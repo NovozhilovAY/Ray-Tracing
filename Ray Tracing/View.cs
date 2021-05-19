b61a9d8ab6da55848c20233f5c930e7fde95d63c
Alexandr <sasha.n191@yandex.ru>
2021-05-19 11:42:12 +0300

    add View class and loadShader method

---
+++ Ray Tracing/View.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ray_Tracing
+{
+    class View
+    {
+        void loadShader(string filename, ShaderType type, int program, int address)
+        {
+            address = GL.CreateShader(type);
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(filename))
+            {
+                GL.ShaderSource(address, sr.ReadToEnd());
+            }
+            GL.CompileShader(address);
+            GL.AttachShader(program, address);
+            Console.WriteLine(GL.GetShaderInfoLog(address));
+        }
+    }
+}
